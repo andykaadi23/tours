9e31d781a81f384550beb2bc7c0c35295db3434f
andyka adi <andykaadi23@gmail.com>
2023-08-11 11:51:00 +0700

    fetch tours

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
